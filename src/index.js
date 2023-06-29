1d6c5b92e8288733817c2296cef950e33d1a9916
yaromatv <122043732+yaromatv@users.noreply.github.com>
2023-06-29 17:43:05 +0200

    architecture

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// DO NOT TYPE HERE
+
+import './js/books-api';
+import './js/header';
+import './js/home';
+import './js/support-ukraine';
+import './js/modal-book-info';
+import './js/shopping-list';
+import './js/footer';
