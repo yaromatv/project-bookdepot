80ddab5d55163b75986795ddfc3b7ec3f8d750fc
yaromatv <122043732+yaromatv@users.noreply.github.com>
2023-07-02 11:02:07 +0200

    scroll up refactor

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,30 +1,14 @@
 const scrollUpBtn = document.querySelector('.scroll-up-btn');
-const rootEl = document.documentElement;
-
-// console.log('hi');
-// console.log('YOYOYO', rootEl.scrollHeight);
-// console.log(rootEl.clientHeight);
-
-document.addEventListener('scroll', () => {
-  console.log(rootEl.scrollHeight);
-  console.log(rootEl.clientHeight);
-});
 
 function onScroll() {
-  // Do something on scroll - 0.15 is the percentage the page has to scroll before the button appears
-  // This can be changed - experiment
-  const scrollTotal = rootEl.scrollHeight - rootEl.clientHeight;
-  if (rootEl.scrollTop / scrollTotal > 0.05) {
-    // Show button
+  if (window.scrollY > 100) {
     scrollUpBtn.classList.add('show-scroll-up-btn');
   } else {
-    // Hide button
     scrollUpBtn.classList.remove('show-scroll-up-btn');
   }
 }
 
 function scrollUp() {
-  // Scroll to top logic
   rootEl.scrollTo({
     top: 0,
     behavior: 'smooth',
