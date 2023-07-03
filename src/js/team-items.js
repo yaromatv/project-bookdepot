26d8a2d2f14ed2289beda6cc708bcff05770e6cf
OlegKryuchkov <kryuchkov.oleg@gmail.com>
2023-07-03 23:47:50 +0300

    updated footer
    Updated footer for Viktoria for adding css classes.

--- src/js/team-items.js
+++ src/js/team-items.js
@@ -1,81 +1,101 @@
+import image1 from '../images/team/Yaroslav_Matvieiev.jpg';
+import image2 from '../images/team/Denys_Lavryk.jpg';
+import image3 from '../images/team/Anastasiia_Vedmid.jpg';
+import image4 from '../images/team/Marina_Sihida.jpg';
+import image5 from '../images/team/Vitalii_Duduka.jpg';
+import image6 from '../images/team/Mary_Kuzo.jpg';
+import image7 from '../images/team/Oleksii_Burlaka.jpg';
+import image8 from '../images/team/Victory_Krasova.jpg';
+import image9 from '../images/team/Artem_Zaicew.jpg';
+import image10 from '../images/team/Oleksii_Khodarchenko.jpg';
+import image11 from '../images/team/oleg_kryuchkov.jpg';
+
 export const team = [
   {
     name: 'Yaroslav Matvieiev',
     position: 'Team Lead, Developer',
     // description: 'JS, HTML, SCSS',
-    img: 'http://static.rezka.cloud/i/2023/6/8/p60ceb629657cdx72x46n.jpg',
+    img: image1,
     url: 'https://github.com/yaromatv',
   },
 
   {
-    name: 'Yaroslav Matvieiev',
-    position: 'Team Lead, Developer',
+    name: 'Denys Lavryk',
+    position: 'Scrum Master, Developer',
     // description: 'JS, HTML, SCSS',
-    img: 'http://static.rezka.cloud/i/2023/6/8/p60ceb629657cdx72x46n.jpg',
-    url: 'https://github.com/yaromatv',
+    img: image2,
+    url: 'https://github.com/strictly-rigid',
   },
 
   {
-    name: 'Yaroslav Matvieiev',
-    position: 'Team Lead, Developer',
+    name: 'Anastasiia Vedmid',
+    position: 'Developer',
     // description: 'JS, HTML, SCSS',
-    img: 'http://static.rezka.cloud/i/2023/6/8/p60ceb629657cdx72x46n.jpg',
-    url: 'https://github.com/yaromatv',
+    img: image3,
+    url: 'https://github.com/neyonse',
   },
 
   {
-    name: 'Yaroslav Matvieiev',
-    position: 'Team Lead, Developer',
+    name: 'Maryna Sihida',
+    position: 'Developer',
     // description: 'JS, HTML, SCSS',
-    img: 'http://static.rezka.cloud/i/2023/6/8/p60ceb629657cdx72x46n.jpg',
-    url: 'https://github.com/yaromatv',
+    img: image4,
+    url: 'https://github.com/marinasigida',
   },
 
   {
-    name: 'Yaroslav Matvieiev',
-    position: 'Team Lead, Developer',
+    name: 'Vitalii Duduka',
+    position: 'Developer',
     // description: 'JS, HTML, SCSS',
-    img: 'http://static.rezka.cloud/i/2023/6/8/p60ceb629657cdx72x46n.jpg',
-    url: 'https://github.com/yaromatv',
+    img: image5,
+    url: 'https://github.com/VitaliiDuduka',
   },
 
   {
-    name: 'Yaroslav Matvieiev',
-    position: 'Team Lead, Developer',
+    name: 'Mary Kuzo',
+    position: 'Developer',
     // description: 'JS, HTML, SCSS',
-    img: 'http://static.rezka.cloud/i/2023/6/8/p60ceb629657cdx72x46n.jpg',
-    url: 'https://github.com/yaromatv',
+    img: image6,
+    url: 'https://github.com/marykuzo',
   },
 
   {
-    name: 'Yaroslav Matvieiev',
-    position: 'Team Lead, Developer',
+    name: 'Oleksii Burlaka',
+    position: 'Developer',
     // description: 'JS, HTML, SCSS',
-    img: 'http://static.rezka.cloud/i/2023/6/8/p60ceb629657cdx72x46n.jpg',
-    url: 'https://github.com/yaromatv',
+    img: image7,
+    url: 'https://github.com/Oleksii001',
   },
 
   {
-    name: 'Yaroslav Matvieiev',
-    position: 'Team Lead, Developer',
+    name: 'Victory Krasova',
+    position: 'Developer',
     // description: 'JS, HTML, SCSS',
-    img: 'http://static.rezka.cloud/i/2023/6/8/p60ceb629657cdx72x46n.jpg',
-    url: 'https://github.com/yaromatv',
+    img: image8,
+    url: 'https://github.com/VictoryKrasova',
   },
 
   {
-    name: 'Yaroslav Matvieiev',
-    position: 'Team Lead, Developer',
+    name: 'Artem Zaicev',
+    position: 'Developer',
     // description: 'JS, HTML, SCSS',
-    img: 'http://static.rezka.cloud/i/2023/6/8/p60ceb629657cdx72x46n.jpg',
-    url: 'https://github.com/yaromatv',
+    img: image9,
+    url: 'https://github.com/Artemzaicew',
   },
 
   {
-    name: 'Yaroslav Matvieiev',
-    position: 'Team Lead, Developer',
+    name: 'Oleksii Khodarchenko',
+    position: 'Developer',
     // description: 'JS, HTML, SCSS',
-    img: 'http://static.rezka.cloud/i/2023/6/8/p60ceb629657cdx72x46n.jpg',
-    url: 'https://github.com/yaromatv',
+    img: image10,
+    url: 'https://github.com/oleksii3339',
+  },
+
+  {
+    name: 'Oleg Kryuchkov',
+    position: 'Developer',
+    // description: 'JS, HTML, SCSS',
+    img: image11,
+    url: 'https://github.com/OlegKryuchkov',
   },
 ];
