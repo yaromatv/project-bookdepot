bbbe5a59b1af261e6b289400ba7d0880af2f0372
MarinaSigida <sigida.web@gmail.com>
2023-07-04 20:51:15 +0200

    Support-Ukraine p.1

---
+++ src/js/fund-list.js
@@ -0,0 +1,75 @@
+import fund1 from '../images/funds-png/SaveTheChildren.png';
+import fund1x from '../images/funds-png/SaveTheChildren-2x.png';
+import fund2 from '../images/funds-png/projectHope.png';
+import fund2x from '../images/funds-png/projectHope-2x.png';
+import fund3 from '../images/funds-png/United24.png';
+import fund3x from '../images/funds-png/United24-2x.png';
+import fund4 from '../images/funds-png/InternationalMedicalCorps.png';
+import fund4x from '../images/funds-png/InternationalMedicalCorps.png';
+import fund5 from '../images/funds-png/MedicinsSansFrontiers.png';
+import fund5x from '../images/funds-png/MedicinsSansFrontiers-2x.png';
+import fund6 from '../images/funds-png/razom.png';
+import fund6x from '../images/funds-png/razom-2x.png';
+import fund7 from '../images/funds-png/ActionAgainstHunger.png';
+import fund7x from '../images/funds-png/ActionAgainstHunger-2x.png';
+import fund8 from '../images/funds-png/WorldVision.png';
+import fund8x from '../images/funds-png/WorldVision-2x.png';
+import fund9 from '../images/funds-png/SergiyPrytula.png';
+import fund9x from '../images/funds-png/SergiyPrytula-2x.png';
+
+export const fundList = [
+    {
+      title: 'Save the Children',
+      url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+      img: fund1,
+      retinaImg: fund1x,
+    },
+    {
+      title: 'Project HOPE',
+      url: 'https://www.projecthope.org/country/ukraine/',
+      img: fund2,
+      retinaImg: fund2x,
+    },
+    {
+      title: 'UNITED24',
+      url: 'https://u24.gov.ua/uk',
+      img: fund3,
+      retinaImg: fund3x,
+    },
+    {
+      title: 'International Medical Corps',
+      url: 'https://internationalmedicalcorps.org/country/ukraine/',
+      img: fund4,
+      retinaImg: fund4x,    
+    },
+    {
+      title: 'Medicins Sans Frontieres',
+      url: 'https://www.msf.org/ukraine',
+      img: fund5,
+      retinaImg: fund5x,
+    },
+    {
+      title: 'RAZOM',
+      url: 'https://www.razomforukraine.org/',
+      img: fund6,
+      retinaImg: fund6x,    
+    },
+    {
+      title: 'Action against hunger',
+      url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+      img: fund7,
+      retinaImg: fund7x,   
+    },
+    {
+      title: 'World vision',
+      url: 'https://www.wvi.org/emergencies/ukraine',
+      img: fund8,
+      retinaImg: fund8x,
+    },
+    {
+      title: 'Serhiy Prytula Charity Foundation',
+      url: 'https://prytulafoundation.org/en',
+      img: fund9,
+      retinaImg: fund9x,
+    },
+  ];
